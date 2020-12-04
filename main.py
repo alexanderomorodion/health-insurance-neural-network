023d092d911c6b8a2c62ccd1692292aeb30976d1
Alex Omorodion <alexanderomorodion@gmail.com>
2020-12-04 16:40:22 -0600

    Created categorical model!

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from Customer import Customer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from numpy import array
+from tensorflow import feature_column
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 from numpy import argmax
 
 
@@ -62,6 +65,29 @@ def onehot_encode(data):
     onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
     return onehot_encoded
 
+def input_fn(features, labels, training=True, batch_size=256):
+    """An input function for training or evaluating"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle and repeat if you are in training mode.
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+
+    return dataset.batch(batch_size)
+
+# A utility method to create a tf.data dataset from a Pandas Dataframe
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop('Response')
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(dataframe))
+    ds = ds.batch(batch_size)
+    return ds
+
+
+'''
 read_data()
 
 
@@ -84,17 +110,85 @@ for i in range(age_matrix.shape[0]):
     customers[i].region = region_matrix[i]
     customers[i].vehicle_age = veh_age_matrix[i]
 
-
 '''
+
 CSV_COLUMN_NAMES = ['Age', 'DrivingLicense', 'Region', 'PreviouslyInsured', 'VehicleAge', 'VehicleDamage',
                     'AnnualPremium', 'PolicySalesChannel', 'Vintage', 'Response']
 RESPONSES = ['Interested', 'Not Interested']
 
-train_path = tf.keras.utils.get_file(
-    "training_data.csv")
-test_path = tf.keras.utils.get_file(
-    "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+dataframe = pd.read_csv("training_data.csv")
+tf.keras.backend.set_floatx('float64')
+dataframe.drop(columns='id')
+train, test = train_test_split(dataframe, test_size=0.15)
+train, val = train_test_split(train, test_size=0.15)
 
-train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
-test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+
+
+feature_columns = []
+
+# numeric cols
+for header in ['Age', 'Driving_License', 'Region_Code', 'Previously_Insured', 'Annual_Premium', 'Policy_Sales_Channel', 'Vintage']:
+    feature_columns.append(feature_column.numeric_column(header))
+
+# bucketized cols
+age = feature_column.numeric_column('Age')
+age_buckets = feature_column.bucketized_column(age, boundaries=[40, 50, 60, 70])
+feature_columns.append(age_buckets)
+
+# indicator cols
+indicator_column_names = ['Gender', 'Vehicle_Age', 'Vehicle_Damage']
+for col_name in indicator_column_names:
+    categorical_column = feature_column.categorical_column_with_vocabulary_list(
+        col_name, dataframe[col_name].unique())
+    indicator_column = feature_column.indicator_column(categorical_column)
+    feature_columns.append(indicator_column)
+
+feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+
+batch_size = 32
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+model = tf.keras.Sequential([
+    feature_layer,
+    layers.Dense(128, activation='relu'),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(.1),
+    layers.Dense(1)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_ds,
+          validation_data=val_ds,
+          epochs=10)
+
+loss, accuracy = model.evaluate(test_ds)
+print("Accuracy", accuracy)
 '''
+#test_path = tf.keras.utils.get_file(
+#    "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+
+#test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+
+
+my_feature_columns = []
+for key in train.keys():
+    my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+
+classifier = tf.estimator.DNNClassifier(
+    feature_columns=my_feature_columns,
+    # One hidden layer of 30 nodes
+    hidden_units=[30],
+    # The model must choose between 2 classes.
+    n_classes=2)
+
+classifier.train(
+    input_fn=lambda: input_fn(train, train_y, training=True),
+    steps=5000)
+    '''
+
+
